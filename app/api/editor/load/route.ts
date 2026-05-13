ab6a4d19e6609853b7ded543fee7d56514ee0ba1
Brayden Middlebrooks <bmidd0170@launchpadphilly.org>
2026-05-13 11:25:47 -0400

    vercel fix for the presentation

--- app/api/editor/load/route.ts
+++ app/api/editor/load/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 import { prisma } from "@/lib/prisma"
-import { jwtDecode } from "jwt-decode"
+import jwtDecode from "jwt-decode"
 
 type LoadRequest = {
   eventId: string
